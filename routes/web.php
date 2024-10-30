4d56761941716c68deb10f35f09dfe276ef1e777
khinoosan <yepaing22222@gmail.com>
2024-10-30 13:56:50 +0900

    aa

--- routes/web.php
+++ routes/web.php
@@ -37,12 +37,10 @@ Route::get('/register',[RegisterController::class,'showRegisterForm'])->name('re
 Route::post('/register',[RegisterController::class,'register']);
 Route::get('/password/reset',[ForgotPasswordController::class, 'showLinkRequestForm'])->name('password.request');
 Route::get('/Event', [EventController::class, 'index']);
-<<<<<<< HEAD
 Route::get('/api/events', [EventController::class, 'getEvents']);
-Route::any('/chat', [ChatController::class, 'chat']);
 Route::get('/Event/{id}', [EventController::class, 'detail'])->name('events.detail');
-=======
->>>>>>> 4d33a7669954975f73a29fc98a13e98a60df853f
+
+
 
 
 
