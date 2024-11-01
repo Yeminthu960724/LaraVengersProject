8183cb662c96a7560b37901f7ef695996273c3e7
khinoosan <yepaing22222@gmail.com>
2024-11-01 16:34:11 +0900

    register

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -29,6 +29,6 @@ class RegisterController extends Controller
             'password' => Hash::make($request->password),
         ]);
 
-        return redirect()->route('Place');  // Redirect to the home page after registration
+        return redirect()->route('Place');
     }
 }
