a5f3034f0df163b6b54d9924be9aaba22144c913
Yeminthu960724 <yepaing22222@gmail.com>
2024-11-11 09:22:29 +0900

    web 修正した

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\PlaceController;
 use App\Http\Controllers\PlaceDetailController;
 use App\Http\Controllers\CartController;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\LoginController;
 use App\Http\Controllers\PlanController;
 use App\Http\Controllers\PlanDetailController;
 use App\Http\Controllers\Auth\RegisterController;
@@ -29,8 +29,11 @@ use App\Http\Controllers\AuthController;
 
 
 
-Route::resource('Place', PlaceController::class);
-
+Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [LoginController::class, 'login'])->name('login');
+Route::get('', [HomeController::class, 'index']);
+// Route::get('/Place', [PlaceController::class, 'index']);
+Route::get('/Place', [PlaceController::class, 'index'])->name('Place');
 Route::get('/PlaceDetail', [PlaceDetailController::class, 'index']);
 Route::get('/Cart', [CartController::class, 'index'])->name('cart.index');
 Route::get('/Plan', [PlanController::class, 'index']);
@@ -42,13 +45,6 @@ Route::get('/Event', [EventController::class, 'index']);
 Route::get('/api/events', [EventController::class, 'getEvents']);
 Route::get('/Event/{id}', [EventController::class, 'detail'])->name('events.detail');
 Route::get('/placeDetail/{id}', [PlaceDetailController::class, 'show'])->name('placeDetail');
-
-Route::get('/myprofile',[ProfileController::class, 'myProfile'])->name('myprofile');
-
-Route::get('/login',[AuthController::class, 'showLoginForm'])->name('login');
-Route::post('/login', [AuthController::class, 'login']);
-
-
 
 
 
